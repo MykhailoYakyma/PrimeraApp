c11132bcfab90a0dc72d31b368da910bbfae2449
MykhailoYakyma <73889022+MykhailoYakyma@users.noreply.github.com>
2021-10-01 15:40:03 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Pol es tonto
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
