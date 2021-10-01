9a52e8c033318542f5c7c50c9d3e618aaa278896
Mykhailo Yakyma <mexayakima1@gmail.com>
2021-10-01 15:28:02 +0200

    commit

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.explosion);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Context context = getApplicationContext();
+                CharSequence text = "FUCK YOU POL!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+    }
+}
